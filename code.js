a4c0cffc80d270ed6c758a7a83ea431c2a3b8017
Nate Kinne <nathan.kinne@gmail.com>
2019-05-18 22:35:19 -0500

    Final version

---
+++ code.js
@@ -0,0 +1,213 @@
+// progress
+let questionNumber = 0;
+// score
+let score = 0;
+
+// quiz started
+//check if start quizz button clicked
+function startQuiz() {
+    $(document).on('click', '.coverPage button', function (event) {
+        console.log('quiz started');
+        removeCover();
+        displayScoreBoard();
+        //display question box
+        $('.questionBox').css('display', 'block');
+        console.log('displaying questionbox');
+    });
+}
+//remove the cover page text
+function removeCover() {
+    //removing cover page
+    console.log('Cover removed');
+    $('.coverPage').remove();
+}
+//display question box
+function displayScoreBoard() {
+    $('.board').css('display', 'block');
+    console.log('displaying board');
+}
+//update score board
+function updateScoreBoard() {
+    console.log('update score board');
+}
+//generate a question
+function generateQuestion() {
+    if (questionNumber < STORE.length) {
+        console.log('Generating question');
+        return `<div class="questions">
+     <h2>${STORE[questionNumber].question}</h2>
+     <div class="pictureContainer">
+      <img src="${STORE[questionNumber].picture}" alt="${STORE[questionNumber].alt}">
+     </div>
+     <form>
+        <fieldset>
+          <label class="answerOption first-answer">
+            <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer">
+            <span>${STORE[questionNumber].answers[0]}</span>
+          </label>
+
+          <label class="answerOption second-answer">
+            <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer">
+            <span>${STORE[questionNumber].answers[1]}</span>
+          </label>
+
+          <label class="answerOption third-answer">
+            <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer">
+            <span>${STORE[questionNumber].answers[2]}</span>
+          </label>
+
+          <label class="answerOption fourth-answer">
+            <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer">
+            <span>${STORE[questionNumber].answers[3]}</span>
+          </label>
+
+        </fieldset>
+        <button type="submit" class="submitbtn">Check Answer</button>
+     </form>
+     </div>`;
+    } else {
+        console.log('end of quiz');
+        renderEndOfQuizPanel();
+        restartQuiz();
+    }
+
+}
+//render a submit button
+//render the generated question
+function renderQuestion() {
+    $('.questionBox').html(generateQuestion());
+}
+
+//submit clicked
+//check if answer is correct
+function getUserAnswer() {
+    $(document).on('submit', 'form', function (event) {
+        event.preventDefault();
+        let selected = $('input:checked');
+        let answer = selected.val();
+        let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+        if (answer === correctAnswer) {
+            //remove previous question
+            $('.questions').remove();
+            feedbackCorrect();
+            updateScoreBoard();
+            // next();
+            console.log('correct');
+        } else {
+            //remove previous question
+            $('.questions').remove();
+            feedbackWrong();
+            console.log('wrong answer');
+        }
+    });
+}
+//increase score
+function updateScoreBoard() {
+    score++;
+    $('#score').text(score);
+    console.log('score board updates');
+}
+//increase question remaining
+function updateQuestion() {
+    questionNumber++;
+    $('#remaining').text(questionNumber + 1);
+    console.log('updating question');
+    console.log(questionNumber);
+
+}
+
+
+
+//feedback
+function feedbackCorrect() {
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    let fact = `${STORE[questionNumber].fact}`;
+    $('.questionBox').append(`<div class="feedback">
+  <h1 id="corrctIncorect">Correct</h1>
+  <p>The correct answer is: </p>
+  <p>${correctAnswer}</p>
+  <p>Fact:</p>
+  <p>${fact}</p>
+  <button class="next-btn">Next Question</button>
+  </div>`);
+}
+
+//feedback for getting feedback on the 
+//user answer
+function feedbackWrong() {
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    let fact = `${STORE[questionNumber].fact}`;
+    $('.questionBox').append(`<div class="feedback">
+  <h1 id="corrctIncorect">wrong</h1>
+  <p>The correct answer is: </p>
+  <p>${correctAnswer}</p>
+  <p>Fact:</p>
+  <p>${fact}</p>
+  <button class="next-btn">Next Question</button>
+  </div>`);
+}
+
+//get new question
+function next() {
+
+    $(document).on('click', '.next-btn', function () {
+        console.log('nextBtn clicked');
+        //remove feedback
+        $('.feedback').remove();
+        //update question remaining
+
+        updateQuestion();
+        //generate new question
+        renderQuestion();
+        console.log('next question loading');
+    });
+}
+
+//end of quiz review
+function endOfQuizPanel(passFail, comment) {
+    return `<div class="review">
+          <h1>Review</h1>
+          <h2 class="passFail">${passFail}</h2>
+          <p>Final Score: <span id="finalScore">${score}</span>/10</p>
+          <p><span class="comments">${comment}</span></p>
+          <button class="restart">Restart</button>
+        </div>`;
+}
+
+//render end of quiz panel check if passed and add comment
+function renderEndOfQuizPanel() {
+    const pass = 'Passed Quiz';
+    const fail = 'Failed Quiz';
+    const passComment = "Great Job! You really know your geography";
+    const failComment = "Better luck next time. Do some review and try again";
+    if (score >= 7) {
+        console.log('passed');
+        $('main').append(endOfQuizPanel(pass, passComment));
+    } else {
+        console.log('failed');
+        $('main').append(endOfQuizPanel(fail, failComment));
+    }
+}
+
+//restart quiz
+function restartQuiz() {
+    $(document).on('click', '.restart', function () {
+        location.reload();
+    });
+}
+
+
+// display answers and mark correct and incorrect
+//reset button
+function createQuiz() {
+    startQuiz();
+    renderQuestion();
+    getUserAnswer();
+    next();
+}
+
+(function () {
+    //start quiz
+    $(createQuiz);
+
+}());
---
+++ store.js
@@ -0,0 +1,112 @@
+const STORE = [
+    {
+        // question 1
+        question: 'What country is Mount Everest in?',
+        answers: [
+            'Nepal', 'India', 'Burma', 'Bahrain'
+        ],
+        correctAnswer: 'Nepal',
+        fact: 'Mount Everest is located in Nepal. 25 of the worlds highest peaks are in the Himilayas',
+        picture: 'https://i.imgur.com/UOj5Q2q.jpg',
+        alt: 'Mount Everest Sunset'
+    },
+    {
+        // question 2
+        question: 'What state capital is impossible to drive to?',
+        answers: [
+            'Boise', 'Juneau', 'Sacramento', 'Pierre'
+        ],
+        correctAnswer: 'Juneau',
+        fact: 'Juneau is the only state capital impossible to drive to. a locked on one side by rugged mountains and by the other sea.',
+        picture: 'https://i.imgur.com/mufJtVz.jpg',
+        alt: 'Downtown Juneau'
+    },
+    {
+        // question 3
+        question: 'You know Egypt has pyramids, what other countries have pyramids?',
+        answers: [
+            'Brazil', 'Costa Rica', 'Italy', 'Venezuela'
+        ],
+        correctAnswer: 'Italy',
+        fact: 'Pyramid of Cestius. The pyramid was constructed between 18BC and 12BC as a tomb for Gaius Cestius, an important Roman magistrate.',
+        picture: 'http://earthporm.com/wp-content/uploads/2014/06/Pyramid_of_cestius.jpg',
+        alt: 'Pirimida hill'
+    },
+    {
+        //question 4
+        question: "Where is the highest sheer cliff?",
+        answers: [
+            'Canada', 'South Africa', 'Nepal', 'Norway'
+        ],
+        correctAnswer: 'Canada',
+        fact: 'Mt Thor Baffin Island has the highest sheer cliff. You can drop 4000ft before hitting anything.',
+        picture: 'https://external-preview.redd.it/SaMfuOf72Q2bRDjtqJKTyQdKTX5ukDXwUK_-_ctIhY0.jpg?auto=webp&s=2031ed4de64633ade13aa9258c9be156573cb559',
+        alt: 'Mt Thor Baffin Island'
+    },
+    {
+        // question 5
+        question: 'Which country is wider than the moon?',
+        answers: [
+            'USA', 'Canada', 'Australia', 'Greenland'
+        ],
+        correctAnswer: 'Australia',
+        fact: 'Australia is not only wider than the moon but also four times wider than pluto',
+        picture: 'https://i.imgur.com/B4TQWEj.jpg',
+        alt: 'Close up picture of the moon'
+    },
+    {
+        // question 6
+        question: 'Which body of water is continuously sinking?',
+        answers: [
+            'Lake Superior', 'Dead Sea', 'Caspian Sea', 'Lake Titicaca'
+        ],
+        correctAnswer: 'Dead Sea',
+        fact: 'The Dead Sea is sinking not losing water. Getting farther below sea level every years.',
+        picture: 'https://i.imgur.com/Fe2XRjS.jpg',
+        alt: 'Dead Sea Spa'
+    },
+    {
+        //question 7
+        question: 'How many time zones does Russia have?',
+        answers: [
+            '13', '9', '11', '10'
+        ],
+        correctAnswer: '11',
+        fact: 'Russia spans 11 time zones. At one end it can be 7am and the other 6pm.',
+        picture: 'http://i.imgur.com/qNUN26u.png',
+        alt: 'Russia outline'
+    },
+    {
+        //Question 8
+        question: 'What is the world\'s smallest country?',
+        answers: [
+            'Andora', 'Monocco', 'Vatican City', 'Nauru'
+        ],
+        correctAnswer: 'Vatican City',
+        fact: 'Vatican City is the smallest Country at only 109 acres. It got its current size from the Lateran treaty of 1929. And if you did not know Vatican means palace of the popes', picture: 'https://i.imgur.com/VLZyzEe.jpg',
+        alt: 'Vatican City'
+    },
+    {
+        //Question 9
+        question: 'How many hemishperes does Africa encompass?',
+        answers: [
+            '4', '6', '3', '2'
+        ],
+        correctAnswer: '4',
+        fact: 'Africa spans every single hemisphere.',
+        picture: 'https://i.imgur.com/n7vqaxz.jpg',
+        alt: 'africa outline'
+    },
+    {
+        //question 10
+        question: 'What country does not have a single river?',
+        answers: [
+            'Qatar', 'Iran', 'Saudi Arabia', 'Egypt'
+        ],
+        correctAnswer: 'Saudi Arabia',
+        fact: 'Saudi Arabia is so dry it does not contain any rivers at all.',
+        picture: 'https://i.pinimg.com/474x/50/8a/49/508a497ecc16ff3c68041fe147595320--desert-sunset-desert-sahara.jpg',
+        alt: 'Sandy Desert'
+    }
+
+];
